cdc6da94bbd92e3754705e52112d68d1968bc3b5
Avi Rana <itzavi1718@gmail.com>
2024-10-08 15:44:29 +0530

    Handled login/signup and server creation

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const uuid = require("uuid");
+const session = require("express-session")
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
+app.use(session({
+    secret:"discord"
+}));
 
 const PORT = 3030;
 
@@ -12,16 +16,12 @@ app.get("/", (req, res)=>{
     res.send("Discord clone!")
 })
 
+// auth
 app.post("/createAccount", (req, res)=>{
     console.log(req.body);
 
-    if(req.body==null)
-        return;
-
-
-    
     let data = JSON.parse(fs.readFileSync("./data.json"));
-    data=data.filter((el)=>{
+    data=data.userData.filter((el)=>{
         return el.email==req.body.email
     });
 
@@ -29,19 +29,71 @@ app.post("/createAccount", (req, res)=>{
         // user already exists
         res.json({type:"ERROR",msg: "User already exists!"});
     } else {
+        // missing tag (#)
         let userObj = {
             email: req.body.email,
             password: req.body.password,
             username: req.body.username,
+            userID: uuid.v4(),
+            pfpURL: null,
             createdAt: Date.now(),
-            onlinePresence: "offline"
+            onlinePresence: "offline",
+            joinedServers:[],
+            friends: []
         }
         data = JSON.parse(fs.readFileSync("./data.json"));
-        data.push(userObj);
+        data.userData.push(userObj);
         fs.writeFileSync("./data.json", JSON.stringify(data));
         res.json({type: "SUCCESS", msg: "Created new user."});
     }
 });
+
+app.post("/loginAccount", (req, res)=>{
+    let data = JSON.parse(fs.readFileSync("./data.json"));
+    data=data.userData.filter((el)=>{
+        return el.email == req.body.email && el.password==req.body.password;
+    })
+
+    if(data.length==0) {
+        //error
+        res.json({type:"ERROR", msg: `Unable to login! Email or password was invalid.`})
+    } else {
+        res.json({type: "SUCCESS", msg: `Logged in as ${data[0].username} (${data[0].userID}).`, res: data[0].joinedServers});
+    }
+});
+
+// server
+
+app.post("/createServer", (req, res)=>{
+    let data = JSON.parse(fs.readFileSync("./data.json"));
+    
+    let serverObj = {
+        name: req.body.name,
+        serverIcon: null,
+        serverID: uuid.v4(),
+        membersList: [req.body.adminID],
+        adminID: req.body.adminID
+    }
+
+    let pos=data.userData.findIndex((el)=>el.userID==req.body.adminID);
+    console.log(pos);
+
+    if(pos==-1) {
+        // error userID not found
+        res.json({type: "ERROR", msg: "Unable to create server! Invalid adminID."})
+    } else {
+        data.userData[pos].joinedServers.push(serverObj.serverID);
+        data.serverData.push(serverObj);
+
+        fs.writeFileSync("./data.json", JSON.stringify(data));
+
+        res.json({type: "SUCCESS", msg: `Server created! ID: ${serverObj.serverID}`});
+    }
+});
+
+// app.post("/joinServer", (req, res)=>{
+// });
+
 
 app.listen(PORT, ()=>{
     console.log("Server started!");
