559510c2132fcf3bf968d03207bb310c3de4da32
ayushdogra-18 <147146430+ayushdogra-18@users.noreply.github.com>
2024-11-13 09:54:29 +0530

    Update page.js

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Page() {
     return (
         <div>
-            <Link href="/auth">Auth</Link>
+            <Link href="/auth" className="m-10">Auth</Link>
             <Link href="/channels/123/232">Chat</Link>
 
         </div>
