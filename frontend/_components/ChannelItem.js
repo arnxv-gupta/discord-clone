3b8d92f6de2536b944be6233af7ec6f3f4a7a617
Avi Rana <itzavi1718@gmail.com>
2024-11-29 23:44:03 +0530

    Update ChannelItem.js

--- frontend/_components/ChannelItem.js
+++ frontend/_components/ChannelItem.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function ChannelItem({name, icon, link}) {
     return (
         <li>
-            <Link href={link} className="px-3 py-2 hover:bg-[#35373C] block rounded-md">
+            <Link href={link} className="px-3 py-2 m-2 hover:bg-[#35373C] block rounded-md">
                 <span className="mr-2">{icon}</span>
                 {name}            
             </Link>
